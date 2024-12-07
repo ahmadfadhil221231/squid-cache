425a47fb74e5d916afe09cf6b61929f955ad3c16
ahmadfadhil221231 <ahmadfadhil221231@gmail.com>
2024-12-07 17:11:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-wiki.squid-cache.org	
+ErrorDocument 404 https://packwooddisposable.com/
+ErrorDocument 500 https://packwooddisposable.com/
+# or map them to one error document:
+# ErrorDocument 404 /pages/errors/error_redirect.php
+# ErrorDocument 500 /pages/errors/error_redirect.php
+
+RewriteEngine On
+RewriteBase /
+
+RewriteCond %{REQUEST_URI} ^/404/$
+RewriteRule ^(.*)$ https://packwooddisposable.com/ [L]
+
+RewriteCond %{REQUEST_URI} ^/500/$
+RewriteRule ^(.*)$ https://packwooddisposable.com/ [L]
+
+# or map them to one error document:
+#RewriteCond %{REQUEST_URI} ^/404/$ [OR]
+#RewriteCond %{REQUEST_URI} ^/500/$
+#RewriteRule ^(.*)$ /pages/errors/error_redirect.php [L]
